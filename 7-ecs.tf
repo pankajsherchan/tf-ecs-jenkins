4f2cb8963652114aa85f036ed64531dd32ed2597
Pankaj  Sherchan <pankajsherchan@Pankajs-MacBook-Pro.local>
2026-05-30 19:54:58 -0700

    Connect ALB to ECS service

--- 7-ecs.tf
+++ 7-ecs.tf
@@ -117,7 +117,11 @@ resource "aws_ecs_service" "nginx" {
     assign_public_ip = false
   }
 
-
+  load_balancer {
+    target_group_arn = aws_lb_target_group.nginx.arn
+    container_name   = "nginx"
+    container_port   = 80
+  }
 }
 
 resource "aws_security_group" "alb" {
@@ -163,3 +167,39 @@ resource "aws_lb" "main" {
     Name = "${local.ecs_name}-alb"
   }
 }
+
+
+resource "aws_lb_target_group" "nginx" {
+  name        = "${local.ecs_name}-nginx-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "${local.ecs_name}-nginx-tg"
+  }
+}
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx.arn
+  }
+}
+
