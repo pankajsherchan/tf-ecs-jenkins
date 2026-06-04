a93369db37409b1715bd058dfb8a2ced71db83e9
Pankaj  Sherchan <pankajsherchan@Pankajs-MacBook-Pro.local>
2026-06-03 20:39:17 -0700

    testing app update

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {
-        "message": "Hello from FastAPI on ECS",
+        "message": "Hello from FastAPI Updated on ECS",
         "status": "ok",
     }
 
